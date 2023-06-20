86584d3359bd8176e6d8ff1fefae9c9e0bbc9440
Sally <sally.foong@sgcarmart.com>
2023-06-20 23:48:45 +0800

    Update index.php

--- qrcode/index.php
+++ qrcode/index.php
@@ -1,6 +1,4 @@
 <?php    
-   
-    
     //set it to writable location, a place for temp generated PNG files
     $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
     
@@ -9,61 +7,70 @@
 
     include "qrlib.php";    
     
-    //ofcourse we need rights to create temp dir
+    //of course, we need rights to create a temp dir
     if (!file_exists($PNG_TEMP_DIR))
         mkdir($PNG_TEMP_DIR);
-    
     
     $filename = $PNG_TEMP_DIR.'test.png';
     
     //processing form input
-    //remember to sanitize user input in real-life solution !!!
+    //remember to sanitize user input in a real-life solution !!!
     $errorCorrectionLevel = 'H';
-
     $matrixPointSize = 2;
 
-
     if (isset($_REQUEST['data']) && isset($_REQUEST['pageno'])) { 
-    
         //it's very important!
         if (trim($_REQUEST['data']) == '')
             die('data cannot be empty! <a href="?">back</a>');
-            echo '<div class="container">';
-            for ($x = 1; $x <= $_REQUEST['pageno']; $x++) {
+        
+        echo '<div class="container">';
+        for ($x = 1; $x <= $_REQUEST['pageno']; $x++) {
             // user data
             $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].$x.'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
             QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);   
             
             echo '<div class="column"><img src="'.$PNG_WEB_DIR.basename($filename).'" />'.'<p>'.$_REQUEST['data'].'+'.$x.' </p></div>';  
-           }
+        }
+        echo '</div>';
+        
+        // Add JavaScript to open the print dialog
+        echo '
+        <script>
+            function printPage() {
+                window.print();
+            }
+            window.onload = printPage;
+        </script>';
+        
+        // Hide the form interface
+        echo '<style>form { display: none; }</style>';
     }
         
-    //display generated file
-    
-    
-    //config form
-    echo '<style>
-    .container {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: center;
-    }
-    .column {
-      flex-basis: 20%;
-      text-align: center;
-      padding: 10px;
-    }
-    .column img {
-      width: 100%;
-      height: auto;
-    }
-    p{
-        font-size:11px;
-        margin:0;
-    }
-  </style><form action="index.php" method="post">
-       <input name="data" value="'.(isset($_REQUEST['data'])?htmlspecialchars($_REQUEST['data']):'PHP QR Code :)').'" />&nbsp';   
-       echo '<input type="tel" name="pageno" id="pageno" class="form-control demo" />';
-       echo '<input type="submit" value="GENERATE"></form>';
-
-       
+    // Display the config form
+    echo '
+        <style>
+            .container {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+            }
+            .column {
+                flex-basis: 20%;
+                text-align: center;
+                padding: 10px;
+            }
+            .column img {
+                width: 100%;
+                height: auto;
+            }
+            p {
+                font-size: 11px;
+                margin: 0;
+            }
+        </style>
+        <form action="index.php" method="post">
+            <input name="data" value="'.(isset($_REQUEST['data'])?htmlspecialchars($_REQUEST['data']):'PHP QR Code :)').'" />&nbsp';
+    echo '<input type="tel" name="pageno" id="pageno" class="form-control demo" />';
+    echo '<input type="submit" value="GENERATE">
+        </form>';
+?>
