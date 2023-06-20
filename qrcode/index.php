cf87cabb186e3fc169a07cd0ff9f221c1ae3d159
Sally <sally.foong@sgcarmart.com>
2023-06-20 23:56:33 +0800

    Update index.php

--- qrcode/index.php
+++ qrcode/index.php
@@ -1,58 +1,34 @@
 <?php
-//set it to writable location, a place for temp generated PNG files
+
+// set it to writable location, a place for temp generated PNG files
 $PNG_TEMP_DIR = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'temp' . DIRECTORY_SEPARATOR;
 
-//html PNG location prefix
+// html PNG location prefix
 $PNG_WEB_DIR = 'temp/';
 
 include "qrlib.php";
 
-//of course we need rights to create temp dir
+// of course, we need rights to create temp dir
 if (!file_exists($PNG_TEMP_DIR)) {
     mkdir($PNG_TEMP_DIR);
 }
 
 $filename = $PNG_TEMP_DIR . 'test.png';
 
-//processing form input
-//remember to sanitize user input in a real-life solution !!!
+// processing form input
+// remember to sanitize user input in a real-life solution !!!
 $errorCorrectionLevel = 'H';
 $matrixPointSize = 2;
 
 if (isset($_REQUEST['data']) && isset($_REQUEST['pageno'])) {
 
-    //it's very important!
+    // it's very important!
     if (trim($_REQUEST['data']) == '') {
         die('data cannot be empty! <a href="?">back</a>');
     }
 
-    echo '<div class="container">';
-
-    for ($x = 1; $x <= $_REQUEST['pageno']; $x++) {
-        // user data
-        $filename = $PNG_TEMP_DIR . 'test' . md5($_REQUEST['data'] . $x . '|' . $errorCorrectionLevel . '|' . $matrixPointSize) . '.png';
-        QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);
-
-        echo '<div class="column"><img src="' . $PNG_WEB_DIR . basename($filename) . '" />' . '<p>' . $_REQUEST['data'] . '+' . $x . ' </p></div>';
-    }
-
-    echo '</div>';
-
-    // hide form interface for printing
-    echo '<script>
-        function printPage() {
-            window.print();
-            setTimeout(function() {
-                window.location.reload();
-            }, 100);
-        }
-        </script>';
-}
-
-// display generated file
-
-// config form
-echo '<style>
+    echo '
+    <style>
     .container {
         display: flex;
         flex-wrap: wrap;
@@ -73,7 +49,71 @@ echo '<style>
     }
     </style>';
 
-echo '<form action="index.php" method="post">
-       <input name="data" value="' . (isset($_REQUEST['data']) ? htmlspecialchars($_REQUEST['data']) : 'PHP QR Code :)') . '" />&nbsp';
-echo '<input type="tel" name="pageno" id="pageno" class="form-control demo" />';
-echo '<input type="submit" value="GENERATE" onclick="printPage(); return false;"></form>';
+    echo '
+    <script>
+    function printPreview() {
+        window.open("print_preview.php", "_blank");
+    }
+    </script>';
+
+    echo '<div id="form-wrapper">';
+
+    echo '<form action="index.php" method="post" onsubmit="printPreview(); return false;">';
+    echo '<input name="data" value="' . (isset($_REQUEST['data']) ? htmlspecialchars($_REQUEST['data']) : 'PHP QR Code :)') . '" />&nbsp';
+    echo '<input type="tel" name="pageno" id="pageno" class="form-control demo" />';
+    echo '<input type="submit" value="GENERATE"></form>';
+
+    echo '</div>';
+
+    echo '<div id="preview-wrapper" style="display: none;">';
+
+    echo '<h1>Print/Preview Page</h1>';
+
+    $data = isset($_POST['data']) ? $_POST['data'] : 'PHP QR Code :)';
+    $pageno = isset($_POST['pageno']) ? intval($_POST['pageno']) : 1;
+
+    echo '<div class="container">';
+    for ($x = 1; $x <= $pageno; $x++) {
+        // user data
+        $filename = $PNG_TEMP_DIR . 'test' . md5($data . $x . '|' . $errorCorrectionLevel . '|' . $matrixPointSize) . '.png';
+        QRcode::png($data, $filename, $errorCorrectionLevel, $matrixPointSize, 2);
+        echo '<div class="column"><img src="' . $PNG_WEB_DIR . basename($filename) . '" /><p>' . $data . '+' . $x . ' </p></div>';
+    }
+    echo '</div>';
+
+    echo '</div>';
+} else {
+    // display the form if no data is submitted
+    echo '
+    <style>
+    #form-wrapper {
+        display: block;
+    }
+    #preview-wrapper {
+        display: none;
+    }
+    </style>';
+
+    echo '<div id="form-wrapper">';
+
+    echo '<form action="index.php" method="post" onsubmit="printPreview(); return false;">';
+    echo '<input name="data" value="' . (isset($_REQUEST['data']) ? htmlspecialchars($_REQUEST['data']) : 'PHP QR Code :)') . '" />&nbsp';
+    echo '<input type="tel" name="pageno" id="pageno" class="form-control demo" />';
+    echo '<input type="submit" value="GENERATE"></form>';
+
+    echo '</div>';
+
+    echo '<div id="preview-wrapper">';
+    echo '<h1>Print/Preview Page</h1>';
+    echo '</div>';
+}
+
+?>
+
+<script>
+window.onload = function() {
+    document.getElementById('form-wrapper').style.display = 'none';
+    document.getElementById('preview-wrapper').style.display = 'block';
+    window.print();
+}
+</script>
