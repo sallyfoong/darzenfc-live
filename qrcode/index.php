b00035768fa7d0852ac017bc89f3558975357b89
Sally <sally.foong@sgcarmart.com>
2023-06-20 23:29:10 +0800

    Update index.php

--- qrcode/index.php
+++ qrcode/index.php
@@ -30,9 +30,9 @@
             die('data cannot be empty! <a href="?">back</a>');
             for ($x = 1; $x <= $_REQUEST['pageno']; $x++) {
             // user data
-            $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
+            $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].$x.'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
             QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);   
-            echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" /><hr/>';  
+            echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" />'.'<br />'.$_REQUEST['data'].'+'.$x;  
            }
     }
         
