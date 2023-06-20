60588130eef893471ba8a2016315b0e951d1240d
Sally <sally.foong@sgcarmart.com>
2023-06-20 23:17:14 +0800

    Update index.php

--- qrcode/index.php
+++ qrcode/index.php
@@ -23,16 +23,16 @@
     $matrixPointSize = 2;
 
 
-    if (isset($_REQUEST['data'])) { 
+    if (isset($_REQUEST['data']) && isset($_REQUEST['pageno'])) { 
     
         //it's very important!
         if (trim($_REQUEST['data']) == '')
             die('data cannot be empty! <a href="?">back</a>');
-            
-        // user data
-        $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
-        QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
-        
+           foreach ($_REQUEST['pageno'] as $each){
+            // user data
+            $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
+            QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
+           }
     }
         
     //display generated file
