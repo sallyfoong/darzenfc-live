5f72aac5badfcd4134e0e84493f128c3fd889224
Sally <sally.foong@sgcarmart.com>
2023-06-20 23:58:42 +0800

    Update index.php

--- qrcode/index.php
+++ qrcode/index.php
@@ -1,119 +1,109 @@
 <?php
 
-// set it to writable location, a place for temp generated PNG files
+//set it to writable location, a place for temp generated PNG files
 $PNG_TEMP_DIR = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'temp' . DIRECTORY_SEPARATOR;
 
-// html PNG location prefix
+//html PNG location prefix
 $PNG_WEB_DIR = 'temp/';
 
 include "qrlib.php";
 
-// of course, we need rights to create temp dir
-if (!file_exists($PNG_TEMP_DIR)) {
+//of course we need rights to create temp dir
+if (!file_exists($PNG_TEMP_DIR))
     mkdir($PNG_TEMP_DIR);
-}
+
 
 $filename = $PNG_TEMP_DIR . 'test.png';
 
-// processing form input
-// remember to sanitize user input in a real-life solution !!!
+//processing form input
+//remember to sanitize user input in real-life solution !!!
 $errorCorrectionLevel = 'H';
 $matrixPointSize = 2;
 
 if (isset($_REQUEST['data']) && isset($_REQUEST['pageno'])) {
 
-    // it's very important!
-    if (trim($_REQUEST['data']) == '') {
+    //it's very important!
+    if (trim($_REQUEST['data']) == '')
         die('data cannot be empty! <a href="?">back</a>');
-    }
-
-    echo '
-    <style>
-    .container {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
-    .column {
-        flex-basis: 20%;
-        text-align: center;
-        padding: 10px;
-    }
-    .column img {
-        width: 100%;
-        height: auto;
-    }
-    p {
-        font-size: 11px;
-        margin: 0;
-    }
-    </style>';
-
-    echo '
-    <script>
-    function printPreview() {
-        window.open("print_preview.php", "_blank");
-    }
-    </script>';
-
-    echo '<div id="form-wrapper">';
-
-    echo '<form action="index.php" method="post" onsubmit="printPreview(); return false;">';
-    echo '<input name="data" value="' . (isset($_REQUEST['data']) ? htmlspecialchars($_REQUEST['data']) : 'PHP QR Code :)') . '" />&nbsp';
-    echo '<input type="tel" name="pageno" id="pageno" class="form-control demo" />';
-    echo '<input type="submit" value="GENERATE"></form>';
-
-    echo '</div>';
-
-    echo '<div id="preview-wrapper" style="display: none;">';
-
-    echo '<h1>Print/Preview Page</h1>';
-
-    $data = isset($_POST['data']) ? $_POST['data'] : 'PHP QR Code :)';
-    $pageno = isset($_POST['pageno']) ? intval($_POST['pageno']) : 1;
 
     echo '<div class="container">';
-    for ($x = 1; $x <= $pageno; $x++) {
-        // user data
-        $filename = $PNG_TEMP_DIR . 'test' . md5($data . $x . '|' . $errorCorrectionLevel . '|' . $matrixPointSize) . '.png';
-        QRcode::png($data, $filename, $errorCorrectionLevel, $matrixPointSize, 2);
-        echo '<div class="column"><img src="' . $PNG_WEB_DIR . basename($filename) . '" /><p>' . $data . '+' . $x . ' </p></div>';
-    }
-    echo '</div>';
 
-    echo '</div>';
-} else {
-    // display the form if no data is submitted
+    for ($x = 1; $x <= $_REQUEST['pageno']; $x++) {
+        // user data
+        $filename = $PNG_TEMP_DIR . 'test' . md5($_REQUEST['data'] . $x . '|' . $errorCorrectionLevel . '|' . $matrixPointSize) . '.png';
+        QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);
+
+        echo '<div class="column"><img src="' . $PNG_WEB_DIR . basename($filename) . '" />' . '<p>' . $_REQUEST['data'] . '+' . $x . ' </p></div>';
+    }
+
+    // Generate JavaScript code to open the print/preview page
+    echo '
+    <script>
+        function printPreview() {
+            // Open a new window for print/preview
+            var printWindow = window.open("", "_blank");
+            var htmlContent = \'<html><head><title>QR Code Print/Preview</title>\';
+            htmlContent += \'<style>body { margin: 0; }</style>\';
+            htmlContent += \'</head><body>\';
+
+            // Append the QR code images to the print window
+            var images = document.querySelectorAll(".column img");
+            for (var i = 0; i < images.length; i++) {
+                var imgSrc = images[i].getAttribute("src");
+                htmlContent += \'<img src="\' + imgSrc + \'" />\';
+            }
+
+            htmlContent += \'</body></html>\';
+
+            // Write the HTML content to the print window
+            printWindow.document.open();
+            printWindow.document.write(htmlContent);
+            printWindow.document.close();
+        }
+    </script>';
+
+    // Hide the form interface and add a button to trigger print/preview
+    echo '
+    <script>
+        function toggleFormVisibility() {
+            var form = document.getElementById("qrForm");
+            form.style.display = form.style.display === "none" ? "block" : "none";
+        }
+    </script>';
+
     echo '
     <style>
-    #form-wrapper {
-        display: block;
-    }
-    #preview-wrapper {
-        display: none;
-    }
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
+        .column {
+            flex-basis: 20%;
+            text-align: center;
+            padding: 10px;
+        }
+        .column img {
+            width: 100%;
+            height: auto;
+        }
+        p {
+            font-size: 11px;
+            margin: 0;
+        }
     </style>';
 
-    echo '<div id="form-wrapper">';
+    echo '
+    <form id="qrForm" action="index.php" method="post" style="display: block;">
+        <input name="data" value="' . (isset($_REQUEST['data']) ? htmlspecialchars($_REQUEST['data']) : 'PHP QR Code :)') . '" />
+        <input type="tel" name="pageno" id="pageno" class="form-control demo" />
+        <input type="submit" value="GENERATE" onclick="toggleFormVisibility(); printPreview(); return false;">
+    </form>';
 
-    echo '<form action="index.php" method="post" onsubmit="printPreview(); return false;">';
-    echo '<input name="data" value="' . (isset($_REQUEST['data']) ? htmlspecialchars($_REQUEST['data']) : 'PHP QR Code :)') . '" />&nbsp';
-    echo '<input type="tel" name="pageno" id="pageno" class="form-control demo" />';
-    echo '<input type="submit" value="GENERATE"></form>';
+    echo '
+    <button onclick="toggleFormVisibility(); printPreview();">Print/Preview</button>';
 
-    echo '</div>';
-
-    echo '<div id="preview-wrapper">';
-    echo '<h1>Print/Preview Page</h1>';
     echo '</div>';
 }
 
 ?>
-
-<script>
-window.onload = function() {
-    document.getElementById('form-wrapper').style.display = 'none';
-    document.getElementById('preview-wrapper').style.display = 'block';
-    window.print();
-}
-</script>
