90c49efc4b1d805fafe1a14b353fc6efa43816d1
Sally <sally.foong@sgcarmart.com>
2023-06-22 17:23:47 +0800

    Update shortenUrl.php

--- shortenUrl.php
+++ shortenUrl.php
@@ -33,7 +33,7 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $longURL = $_POST['longURL'];
         echo $longURL;
-        $shortURL = googleShortURL($longURL);
+        $shortURL = generateGoogleDynamicLink($longURL);
         echo $shortURL;
         echo "<br><input type='text' id='shortURL' value='$shortURL' readonly>";
         echo "<button onclick='copyToClipboard()'>Copy URL</button>";
@@ -65,6 +65,44 @@
         $firebaseShortURL = isset($decodeResult->shortLink) ? $decodeResult->shortLink : '';
         return $firebaseShortURL;
     }
+    function generateGoogleDynamicLink($link) {
+        $apiEndpoint = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyDfc6efTLP-OEkPmSBikxSd-av2YVH4aPU'; // Replace with your API endpoint and API key
+    
+        $postData = array(
+            'dynamicLinkInfo' => array(
+                'domainUriPrefix' => 'https://beyourdi.page.link', // Replace with your dynamic links domain
+                'link' => $link,
+                'fallbackLink' => "https://www.beyourdiary.co/",
+            ),
+            'suffix' => array(
+                'option' => 'SHORT',
+            ),
+        );
+    
+       
+        $postData['dynamicLinkInfo']['parameters'] = strtotime(date("Y-m-d h:i:sa"));
+        
+    
+        $ch = curl_init($apiEndpoint);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    
+        curl_close($ch);
+    
+        if ($httpCode == 200) {
+            $responseData = json_decode($response, true);
+            return $responseData['shortLink'];
+        } else {
+            // Handle the error case
+            return null;
+        }
+    }
+    
     ?>
 </body>
 
