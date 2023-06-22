717fd8f87a8c8f0f3c85ce0f498ddb6b87654861
Sally <sally.foong@sgcarmart.com>
2023-06-22 18:07:49 +0800

    Update shortenUrl.php

--- shortenUrl.php
+++ shortenUrl.php
@@ -1,91 +1,43 @@
-<!DOCTYPE html>
+<?php 
+$host="127.0.0.1:3306";
+$username="darzenfc_shorten_url";
+$password="Sallyfoong1997@";
+$databasename="darzenfc_shorten_url";
+$connect = @mysqli_connect($host,$username,$password,$databasename);
+
+?>
 <html>
+<script>
+function copyToClipboard() {
+    /* Get the text field */
+    var copyText = document.getElementById("shortURL");
 
-<head>
-    <title>Shorten URL</title>
-    <script>
-    function copyToClipboard() {
-        /* Get the text field */
-        var copyText = document.getElementById("shortURL");
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-        /* Select the text field */
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
 
-        /* Copy the text inside the text field */
-        document.execCommand("copy");
-
-        /* Alert the copied text */
-        alert("URL copied to clipboard!");
-    }
-    </script>
-</head>
+    /* Alert the copied text */
+    alert("URL copied to clipboard!");
+}
+</script>
 
 <body>
-    <h1>Shorten URL</h1>
-    <form action="" method="POST">
-        <label for="longURL">Enter URL:</label>
-        <input type="text" id="longURL" name="longURL" placeholder="Enter a long URL" required>
-        <input type="submit" value="Shorten">
+    <form method="post" action="">
+        <input type="text" name="url_value" placeholder="Enter URL">
+        <input type="submit" name="original_url">
     </form>
-
-    <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $longURL = $_POST['longURL'];
-        echo $longURL;
-        $shortURL = generateGoogleDynamicLink($longURL);
-        echo $shortURL;
-        echo "<br><input type='text' id='shortURL' value='$shortURL' readonly>";
+    <?php 
+    if(isset($_POST['url_value'])){
+        $url=$_POST["url_value"];
+        $short_url=substr(md5($url.mt_rand()),0,8);
+        $qrsql = "INSERT INTO short_urls (long_url, short_url)VALUES('$url','$short_url')";  
+        $rsqrsql = @mysqli_query($connect,$qrsql);  
+        echo "<br><input type='text' id='shortURL' value='$short_url' readonly>";
         echo "<button onclick='copyToClipboard()'>Copy URL</button>";
     }
-
-    function generateGoogleDynamicLink($link) {
-        // API endpoint and API key
-        $apiEndpoint = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=YOUR_API_KEY';
-    
-        $postData = array(
-            'dynamicLinkInfo' => array(
-                'domainUriPrefix' => 'https://beyourdiary.page.link',
-                'link' => $link,
-                'androidInfo' => array(
-                    'androidPackageName' => 'beyourdiary.page.link.android',
-                ),
-                'iosInfo' => array(
-                    'iosBundleId' => 'beyourdiary.page.link.ios',
-                ),
-                'navigationInfo' => array(
-                    'enableForcedRedirect' => true,
-                ),
-                'fallbackLink' => "https://www.beyourdiary.co/",
-            ),
-            'suffix' => array(
-                'option' => 'SHORT',
-            ),
-        );
-    
-        $postData['dynamicLinkInfo']['parameters'] = strtotime(date("Y-m-d h:i:sa"));
-    
-        $ch = curl_init($apiEndpoint);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    
-        $response = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    
-        curl_close($ch);
-    
-        if ($httpCode == 200) {
-            $responseData = json_decode($response, true);
-            return $responseData['shortLink'];
-        } else {
-            // Handle the error case
-            return null;
-        }
-    }
-    
-    
     ?>
 </body>
 
