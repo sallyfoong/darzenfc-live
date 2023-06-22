e379a0f3c87d68a65ba11a3286a8d68417614f24
Sally <sally.foong@sgcarmart.com>
2023-06-22 23:23:28 +0800

    Update index.php

--- qrcode/index.php
+++ qrcode/index.php
@@ -27,11 +27,15 @@ $matrixPointSize = 2;
 
 
 if (isset($_REQUEST['productName']) && isset($_REQUEST['pageno'])) {
-    $datainfo="SELECT barcode_prefix, barcode_next_number FROM projects WHERE id='1'";
-    $rowRetrieveInfo = mysqli_query($connect, $datainfo);
-    $data = mysqli_fetch_assoc($rowRetrieveInfo);
-    $barcode_next_number = $data['barcode_next_number'];
-    echo '<div class="container">';
+
+//it's very important!
+if (trim($_REQUEST['productName']) == '')
+die('data cannot be empty! <a href="?">back</a>');
+$datainfo="SELECT barcode_prefix, barcode_next_number FROM projects WHERE id='1'";
+$rowRetrieveInfo = mysqli_query($connect, $datainfo);
+$data = mysqli_fetch_assoc($rowRetrieveInfo);
+$barcode_next_number = $data['barcode_next_number'];
+echo '<div class="container">';
     for ($x = $barcode_next_number; $x
     <= $_REQUEST['pageno']; $x++) { // user data
         $filename=$PNG_TEMP_DIR.'test'.md5($_REQUEST['productName'].$x.'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
@@ -48,9 +52,7 @@ if (isset($_REQUEST['productName']) && isset($_REQUEST['pageno'])) {
             }
         </script>';
     }
-    $finalBarcodeNo = $barcode_next_number + $_REQUEST['pageno'];
-    $sqlupd = "UPDATE projects SET barcode_next_number = '".$finalBarcodeNo."' WHERE id = '1'";
-    $query2 = mysqli_query($connect,$sqlupd); 
+    
 }
 
 //display generated file
