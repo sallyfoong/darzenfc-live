30e7b7d775347de99df72a8e5d9701272a33299f
Sally <sally.foong@sgcarmart.com>
2023-06-22 23:14:33 +0800

    Update index.php

--- qrcode/index.php
+++ qrcode/index.php
@@ -33,9 +33,10 @@ if (isset($_REQUEST['productName']) && isset($_REQUEST['pageno'])) {
     $barcode_next_number = $data['barcode_next_number'];
     echo '<div class="container">';
     for ($x = $barcode_next_number; $x
-    <= $_REQUEST['pageno']; $x++) { // user data
+    <= $_REQUEST['pageno']; $x++) { 
+        $urlRtn = "https://darzenfc.xyz/cms/html/generateBarcode.php?barcode=".$x;
         $filename=$PNG_TEMP_DIR.'test'.md5($_REQUEST['productName'].$x.'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
-        QRcode::png($_REQUEST['productName'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);
+        QRcode::png($urlRtn, $filename, $errorCorrectionLevel, $matrixPointSize, 2);
         echo '<div class="column"><img src="' .$PNG_WEB_DIR.basename($filename).'" />'.'<p>'.$_REQUEST['productName'].' '.$x.'
             </p>
         </div>';// Automatically trigger the print action using JavaScript
