4a27b5c5696f5328b6d493fd3713c1a7c9570f72
Sally <sally.foong@sgcarmart.com>
2023-06-23 23:38:53 +0800

    Update stockRecord.php

--- stockRecord.php
+++ stockRecord.php
@@ -47,79 +47,12 @@ if($barcode && $productId && $warehouseId){
                     $query2 = mysqli_query($connect,$sqlupd); 
 
                     if($query2){
+                        $lastInsertedId = mysqli_insert_id($connect);
+
                         // echo '<script language="javascript">';
                         // echo 'alert("Insert");';
                         // echo '</script>';
-                            // Success step 2
-                            echo '<script language="javascript">';
-                            echo 'let selectedPerson = null;';
-                            echo 'let productBatchCode = null;';
-                            echo 'function selectPerson(person) {';
-                            echo '    selectedPerson = person;';
-                            echo '}';
-                            echo 'function submitForm() {';
-                            echo '    productBatchCode = document.getElementById("batch_code").value;';
-                            echo '    if (selectedPerson && productBatchCode) {';
-                            echo '        // Update stock_record table with the selected values';
-                            echo '        $.ajax({';
-                            echo '            type: "POST",';
-                            echo '            url: "update_stock_record.php", // Replace with the actual PHP file to handle the update';
-                            echo '            data: {';
-                            echo '                stock_in_person_in: selectedPerson,';
-                            echo '                product_batch_code: productBatchCode,';
-                            echo '                barcode: "'.$barcode.'",';
-                            echo '                productId: "'.$productId.'",';
-                            echo '                warehouseId: "'.$warehouseId.'"';
-                            echo '            },';
-                            echo '            success: function(response) {';
-                            echo '                // Handle the success response';
-                            echo '                alert("Stock record updated successfully!");';
-                            echo '            },';
-                            echo '            error: function(xhr, status, error) {';
-                            echo '                // Handle the error response';
-                            echo '                alert("Error updating stock record: " + error);';
-                            echo '            }';
-                            echo '        });';
-                            echo '    } else {';
-                            echo '        alert("Please select a person and enter the product batch code.");';
-                            echo '    }';
-                            echo '}';
-                            echo '</script>';
-                            echo '<div class="modal fade" id="stockInModal" tabindex="-1" role="dialog" aria-labelledby="stockInModalLabel" aria-hidden="true">';
-                            echo '    <div class="modal-dialog" role="document">';
-                            echo '        <div class="modal-content">';
-                            echo '            <div class="modal-header">';
-                            echo '                <h5 class="modal-title" id="stockInModalLabel">Stock In Details</h5>';
-                            echo '                <button type="button" class="close" data-dismiss="modal" aria-label="Close">';
-                            echo '                    <span aria-hidden="true">&times;</span>';
-                            echo '                </button>';
-                            echo '            </div>';
-                            echo '            <div class="modal-body">';
-                            echo '                <div class="form-group">';
-                            echo '                    <label for="person">Stock In Person:</label>';
-                            echo '                    <select class="form-control" id="person" onchange="selectPerson(this.value)">';
-                            echo '                        <option value="">Select Person</option>';
-                            echo '                        <option value="A PERSON">A PERSON</option>';
-                            echo '                        <option value="B PERSON">B PERSON</option>';
-                            echo '                        <option value="C PERSON">C PERSON</option>';
-                            echo '                    </select>';
-                            echo '                </div>';
-                            echo '                <div class="form-group">';
-                            echo '                    <label for="batch_code">Product Batch Code:</label>';
-                            echo '                    <input type="text" class="form-control" id="batch_code" placeholder="Enter Product Batch Code">';
-                            echo '                </div>';
-                            echo '            </div>';
-                            echo '            <div class="modal-footer">';
-                            echo '                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>';
-                            echo '                <button type="button" class="btn btn-primary" onclick="submitForm()">Submit</button>';
-                            echo '            </div>';
-                            echo '        </div>';
-                            echo '    </div>';
-                            echo '</div>';
-                            echo '<script>';
-                            echo '    $("#stockInModal").modal("show");';
-                            echo '</script>';
-                        
+                        header('Location: update_stock_record.php?id='.$lastInsertedId);
                     //product_batch_code
                     //stock_in_person_in
                     }else{
